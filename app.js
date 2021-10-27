b9ee6eb8fd5ea97bc02af167d654ab88cb04a090
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-27 16:15:24 +0200

    create app logic

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const PORT = 3000
+
+// db connection
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+mongoose.connection.on('error', (err) => {
+    console.log('err', err)
+})
+mongoose.connection.on('connected', (err, res) => {
+    console.log('mongoose is connected')
+})
+
+// routes
+const routes = require('./routes/router')
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log('Listerning on port:', PORT)
+})
