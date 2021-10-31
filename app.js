eab97cf477855e6fd4ba9baaa13de1188ead97b3
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-31 18:25:44 +0100

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+
+app.use(cors({ origin: '*' }))
 
 // db connection
 mongoose.connect(process.env.DB_CONNECTION, {
